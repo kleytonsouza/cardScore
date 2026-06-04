8705b7ed2a87d964c20ce056183d12c2fc2d00f9
kleytonsouza <kleytonl.souza@gmail.com>
2026-06-04 18:09:35 -0300

    add tela truco e outros jogos

---
+++ app/src/main/java/com/example/cardscore/TrucoActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cardscore
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TrucoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_truco)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
